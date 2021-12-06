47e5bda69e4840c71ace65a9cfe7506aab87c1c5
dcota <duartenunocota@gmail.com>
2021-12-06 09:25:46 -0100

    fst commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const connection = require('./dbconnection')
+app.use(express.static('./public'))
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/navbar',(req,res)=>{
+    res.sendFile(path.join(__dirname, './public/navbar.html'))
+})
+
+const port =3000
+ 
+app.listen(port, () => {
+    console.log(`Listenning on port ${port}`)
+})
